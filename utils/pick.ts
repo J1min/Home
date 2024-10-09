a103b09a51f8307eca26321f39175960ca289b17
J1min <aoiharu41@gmail.com>
2024-10-09 21:39:32 +0900

    123

--- utils/pick.ts
+++ utils/pick.ts
@@ -4,7 +4,7 @@ import { PickType } from "@/types/pick";
  * 주어진 배열에서 랜덤으로 n개의 요소를 선택하는 함수
  * @param {T[]} arr - 요소를 선택할 배열
  * @param {number} n - 선택할 요소의 수
- * @returns {T[]} 랜덤으로 선택된 n개의 요소로 이루어진 배열
+ * @returns {T[]} - 랜덤으로 선택된 n개의 요소로 이루어진 배열
  */
 export function getRandomElements<T>(arr: T[], n: number = 3): T[] {
   const shuffled = arr.slice();
